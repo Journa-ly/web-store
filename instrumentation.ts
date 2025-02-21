e05b3f8484dea3d0cd371154dc84fd7514ebcd1e
Derry <nixondc93@gmail.com>
2025-02-20 22:33:39 -0500

    remove instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,12 +1,12 @@
 
-export async function register() {
-	const { registerHighlight } = await import('@highlight-run/next/server');
-  const highlightProjectId = String(process.env.HIGHLIGHT_PROJECT_ID);
-  const environment = String(process.env.NEXT_PUBLIC_ENVIRONMENT);
+// export async function register() {
+// 	const { registerHighlight } = await import('@highlight-run/next/server');
+//   const highlightProjectId = String(process.env.HIGHLIGHT_PROJECT_ID);
+//   const environment = String(process.env.NEXT_PUBLIC_ENVIRONMENT);
 
-	registerHighlight({
-    projectID: highlightProjectId,
-    serviceName: "Journa-Web-Store",
-    environment: environment,
-	})
-}
+// 	registerHighlight({
+//     projectID: highlightProjectId,
+//     serviceName: "Journa-Web-Store",
+//     environment: environment,
+// 	})
+// }
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import { NextConfig } from 'next';
 const { withHighlightConfig } = require('@highlight-run/next/config');
 
 const config: NextConfig = {
-  instrumentationHook: true,
   images: {
     unoptimized: true,
     formats: ['image/avif', 'image/webp'],
